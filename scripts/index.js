177ea64c5b77b8377a078f55b38198a199b655db
dung5409p <dungt4141@gmail.com>
2026-02-10 23:01:50 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,4 @@
 /*** CONSTANT ***/
-const COLS = 10;
-const ROWS = 20;
-const BLOCK_SIZE = 30;
 const COLOR_MAPPING = [
   "red",
   "orange",
@@ -187,9 +184,16 @@ const KEY_CODES = {
 
 const WHITE_COLOR_ID = 7;
 
+const COLS = 10;
+const ROWS = 20;
+
+// Tính toán kích thước ô dựa trên chiều rộng màn hình
+const BLOCK_SIZE = Math.floor(window.innerWidth / COLS / 1.5);
+
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
+// Canvas sẽ vừa với màn hình
 ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
